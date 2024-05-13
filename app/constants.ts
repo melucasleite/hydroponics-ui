c844c13dd2c553ab0b3496caecc15724f2a533c4
Lucas Leite <me@lucasleite.us>
2024-05-13 11:36:45 -0400

    refactor: Update temperature and pH thresholds in constants.ts

--- app/constants.ts
+++ app/constants.ts
@@ -1,4 +1,4 @@
-export const LOW_TEMP_THRESHOLD = 25
-export const HIGH_TEMP_THRESHOLD = 35
-export const LOW_PH_THRESHOLD = 4
-export const HIGH_PH_THRESHOLD = 6.5
+export const LOW_TEMP_THRESHOLD = 1
+export const HIGH_TEMP_THRESHOLD = 4
+export const LOW_PH_THRESHOLD = 1
+export const HIGH_PH_THRESHOLD = 4
