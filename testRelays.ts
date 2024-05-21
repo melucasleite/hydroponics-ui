e1d4c0aa3d9c7fdfa301194fd0d1a01ed7e9bd75
Lucas Leite <me@lucasleite.us>
2024-05-20 21:54:35 -0400

    add testRelays

---
+++ testRelays.ts
@@ -0,0 +1,37 @@
+import { SerialPort } from 'serialport';
+import { ReadlineParser } from '@serialport/parser-readline';
+
+async function detectArduinoMega(): Promise<string | undefined> {
+    const arduinoMegaVendorId = '2341';
+    const arduinoMegaProductId = '0042';
+
+    const ports = await SerialPort.list();
+    for (const port of ports) {
+        if (port.vendorId === arduinoMegaVendorId && port.productId === arduinoMegaProductId) {
+            return port.path
+        }
+    }
+    return undefined;
+}
+
+// @ts-ignore
+const path = await detectArduinoMega();
+
+if (!path) {
+    console.error('Arduino Mega not found');
+    process.exit(1);
+}
+
+const port = new SerialPort({ path, baudRate: 115200 });
+
+const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
+
+console.log("Testing relays...")
+
+port.write('testRelays\n');
+
+const log = (data: string) => {
+    console.log('Received data:', data);
+}
+
+parser.on('data', log);
