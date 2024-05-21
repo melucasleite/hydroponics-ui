a81e65c73fe786e8b90931eee796886d4b02cc32
Lucas Leite <me@lucasleite.us>
2024-05-20 21:52:06 -0400

    add testRelays

--- hydroponics-ui-firmware/src/main.cpp
+++ hydroponics-ui-firmware/src/main.cpp
@@ -32,5 +32,16 @@ void loop() {
       readings += "-T1V" + String(int(tempC*10)) + "\n";
       Serial.print(readings);
     }
+    if (command == "testRelays") {
+      for (int i = 7; i <= 13; i++) {
+        pinMode(i, OUTPUT);
+        digitalWrite(i, HIGH);
+        Serial.println("Relay " + String(i) + " is ON");
+        delay(1000);
+        digitalWrite(i, LOW);
+        delay(1000);
+
+      }
+    }
   }
 }
